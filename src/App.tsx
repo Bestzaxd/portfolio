091dea4dba31efd74532256bbf5bd7c070c012b4
bestzaxd <soranabest123@gmail.com>
2024-03-02 00:39:11 +0700

    delete Hero
    delete Hero

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import {
   Contact,
   Experience,
   Feedbacks,
-  Hero,
   Navbar,
   Tech,
   Works,
@@ -26,7 +25,6 @@ const App = () => {
       <div className="bg-primary relative z-0">
         <div className="bg-hero-pattern bg-cover bg-center bg-no-repeat">
           <Navbar />
-          <Hero />
         </div>
         <About />
         <Experience />
